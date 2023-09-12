d671f702a28910ba6d1a7a2c61c2528df92320cc
tonkaew131 <nongtonkaew@gmail.com>
2023-09-12 20:18:17 +0700

    Conjugate: Added Matrix A & B Input

--- src/routes/linear/conjugate.svelte
+++ src/routes/linear/conjugate.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import Label from '$lib/components/ui/label/label.svelte';
 
@@ -12,11 +13,30 @@
 	<Input bind:value={matrixSize} type="number" min="1" class="w-40" />
 </Label>
 
-<Label>
-	Matrix A
-	<div>
-		{#each Array.from(Array(10).keys()) as i}
-			<Input class="aspect-square"/>
-		{/each}
-	</div>
-</Label>
+<div class="flex items-center gap-2 mt-2">
+	<Label>
+		Matrix A
+		<div
+			class="grid auto-cols-auto gap-1 mt-2"
+			style={`grid-template-columns: repeat(${matrixSize}, minmax(0, 3rem));`}
+		>
+			{#each Array.from(Array(Math.pow(matrixSize, 2)).keys()) as i}
+				<Input class="h-12 w-12" />
+			{/each}
+		</div>
+	</Label>
+
+	<Label
+		>Matrix B
+		<div
+			class="grid auto-cols-auto gap-1 mt-2"
+			style={`grid-template-columns: repeat(${1}, minmax(0, 3rem));`}
+		>
+			{#each Array.from(Array(matrixSize).keys()) as i}
+				<Input class="h-12 w-12" />
+			{/each}
+		</div>
+	</Label>
+</div>
+
+<Button>Calculate!</Button>
