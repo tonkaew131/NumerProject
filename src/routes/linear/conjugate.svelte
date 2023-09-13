207045df0e78b3606a8005a61de76b5a0b123fbf
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-09-13 08:25:38 +0700

    Added reset btn in conjugate

--- src/routes/linear/conjugate.svelte
+++ src/routes/linear/conjugate.svelte
@@ -3,15 +3,25 @@
 	import Input from '$lib/components/ui/input/input.svelte';
 	import Label from '$lib/components/ui/label/label.svelte';
 
+	import Icon from '@iconify/svelte';
+
 	let matrixSize: number = 3;
 </script>
 
 <h3 class="">🥹 Conjugate Gradient Methods</h3>
 
-<Label>
-	Matrix size (NxN)
-	<Input bind:value={matrixSize} type="number" min="1" class="w-40" />
-</Label>
+<div class="flex items-end gap-2">
+	<Label>
+		Matrix size (NxN)
+		<Input
+			bind:value={matrixSize}
+			type="number"
+			min="1"
+			class="w-40 placeholder:text-gray-300 mt-2"
+		/>
+	</Label>
+	<Button variant="destructive"><Icon icon="bx:reset" class="text-xl" /></Button>
+</div>
 
 <div class="flex items-center gap-2 mt-2">
 	<Label class="text-center">
@@ -22,7 +32,7 @@
 		>
 			{#each Array(Math.pow(matrixSize, 2)) as _, i (`matrix_a_${i}`)}
 				<Input
-					class="h-20 w-20 text-center"
+					class="h-20 w-20 text-center placeholder:text-gray-300"
 					placeholder={`a${Math.floor(i / matrixSize) + 1}${(i % matrixSize) + 1}`}
 				/>
 			{/each}
@@ -36,7 +46,11 @@
 			style="grid-template-columns: repeat(1, minmax(0, 5rem));"
 		>
 			{#each Array(Number(matrixSize)) as _, i (`matrix_x_${i}`)}
-				<Input class="h-20 w-20 text-center" disabled value={`x${i + 1}`} />
+				<Input
+					class="h-20 w-20 text-center placeholder:text-gray-300"
+					disabled
+					value={`x${i + 1}`}
+				/>
 			{/each}
 		</div>
 	</Label>
@@ -50,7 +64,7 @@
 			style="grid-template-columns: repeat(1, minmax(0, 5rem));"
 		>
 			{#each Array(Number(matrixSize)) as _, i (`matrix_b_${i}`)}
-				<Input class="h-20 w-20 text-center" placeholder={`b${i + 1}`} />
+				<Input class="h-20 w-20 text-center placeholder:text-gray-300" placeholder={`b${i + 1}`} />
 			{/each}
 		</div>
 	</Label>
@@ -63,9 +77,9 @@
 		style={`grid-template-columns: repeat(${matrixSize}, minmax(0, 5rem));`}
 	>
 		{#each Array(Number(matrixSize)) as _, i (`initial_x_${i}`)}
-			<Input class="h-20 w-20 text-center" placeholder={`x${i + 1}`} />
+			<Input class="h-20 w-20 text-center placeholder:text-gray-300" placeholder={`x${i + 1}`} />
 		{/each}
 	</div>
 </Label>
 
-<Button>Calculate!</Button>
+<Button class="mt-2">Calculate!</Button>
