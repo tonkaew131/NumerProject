d43056645d2ef4589224bd36e8cbcbdda0a846f0
tonkaew131 <nongtonkaew@gmail.com>
2023-09-17 23:22:53 +0700

    Fixed build warning

--- src/routes/bookmark/+page.svelte
+++ src/routes/bookmark/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	// import type { PageData } from './$types';
 
-	export let data: PageData;
+	// export let data: PageData;
 </script>
 
-<div>
-    Hi
-</div>
+<div>Hi</div>
