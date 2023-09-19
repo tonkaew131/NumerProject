1d52a645d20cd6ed886f879e9079fe3aeb44d399
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-09-19 16:15:54 +0700

    Added value binding

--- src/routes/inter/newton/newton.svelte
+++ src/routes/inter/newton/newton.svelte
@@ -1,15 +1,54 @@
 <script lang="ts">
+	import Checkbox from '$lib/components/ui/checkbox/checkbox.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
+	import Toggle from '$lib/components/ui/toggle/toggle.svelte';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import Label from '$lib/components/ui/label/label.svelte';
 	import * as Card from '$lib/components/ui/card';
 
-	import Icon from '@iconify/svelte';
-	import Checkbox from '$lib/components/ui/checkbox/checkbox.svelte';
-	import Toggle from '$lib/components/ui/toggle/toggle.svelte';
 	import KaTex from '$lib/components/KaTex.svelte';
 
+	import Icon from '@iconify/svelte';
+
 	let pointSize = 3;
+
+	let points: {
+		[key: number]: {
+			x: number;
+			y: number;
+		};
+	} = {};
+	let selectedPoint: number[] = [];
+
+	function onInputX(e: InputEvent, idx: number) {
+		const target = e.target as HTMLInputElement;
+		const value = Number(target.value);
+
+		points[idx] = {
+			...points[idx],
+			x: value
+		};
+	}
+
+	function onInputY(e: InputEvent, idx: number) {
+		const target = e.target as HTMLInputElement;
+		const value = Number(target.value);
+
+		points[idx] = {
+			...points[idx],
+			y: value
+		};
+	}
+
+	function onSelectPoint(idx: number, val: boolean) {
+		if (val) {
+			selectedPoint.push(idx);
+		} else {
+			selectedPoint = selectedPoint.filter((i) => i !== idx);
+		}
+
+		selectedPoint = [...selectedPoint];
+	}
 </script>
 
 <svelte:head>
@@ -51,17 +90,33 @@
 
 <Card.Root class="w-fit mt-2">
 	<Card.Content class="w-fit py-5">
-		<div class="flex flex-col gap-1 w-fit">
-			{#each Array(pointSize) as _, idx}
-				<div class="flex items-center gap-1 w-fit">
-					<Checkbox />
-					<span class="w-8 text-center">
-						{idx + 1}.
-					</span>
-					<Input type="number" class="w-48 bg-white" placeholder={`x${idx}`} />
-					<Input type="number" class="w-48 bg-white" placeholder={`f(x${idx})`} />
-				</div>
-			{/each}
+		<div class="flex">
+			<div class="flex flex-col gap-1 justify-around">
+				{#each Array(pointSize) as _, idx}
+					<Checkbox onCheckedChange={(val) => onSelectPoint(idx, val)} />
+				{/each}
+			</div>
+			<div class="flex flex-col gap-1 w-fit">
+				{#each Array(pointSize) as _, idx}
+					<div class="flex items-center gap-1 w-fit">
+						<span class="w-8 text-center">
+							{idx + 1}.
+						</span>
+						<Input
+							type="number"
+							class="w-48 bg-white"
+							placeholder={`x${idx}`}
+							on:input={(e) => onInputX(e, idx)}
+						/>
+						<Input
+							type="number"
+							class="w-48 bg-white"
+							placeholder={`f(x${idx})`}
+							on:input={(e) => onInputY(e, idx)}
+						/>
+					</div>
+				{/each}
+			</div>
 		</div>
 		<Toggle variant="outline" class="mt-2">Toggle all</Toggle>
 	</Card.Content>
