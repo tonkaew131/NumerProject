7f846cfe8b05782d63922d1be897a8ca50ac7264
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-10-11 09:22:13 +0700

    Added one-points solutions

---
+++ src/lib/solutions/onePoint.ts
@@ -0,0 +1,50 @@
+import { abs, evaluate } from 'mathjs';
+
+export interface OnePointIterationResult {
+	result: number;
+	iter: number;
+	iterations?: { x: number; y: number }[];
+	error?: string;
+}
+
+export function onePointIteration(
+	xStart: number,
+	errorFactor: number,
+	func: string
+): OnePointIterationResult {
+	const result: OnePointIterationResult = { result: 0, iter: 0, iterations: [] };
+
+	try {
+		evaluate(func, { x: xStart });
+	} catch (error) {
+		result.error = 'Invalid function';
+		return result;
+	}
+
+	let iter = 0;
+	const MAX_ITER = 100;
+	let x = xStart;
+	let xold = x * 100;
+	while (iter < MAX_ITER) {
+		iter += 1;
+		if (iter == MAX_ITER) {
+			result.error = 'Max iteration reached';
+			break;
+		}
+
+		if (result.iterations) result.iterations.push({ x: x, y: evaluate(func, { x: x }) });
+		x = evaluate(func, { x: x });
+
+		if (abs((x - xold) / xold) < errorFactor) {
+			result.result = x;
+			result.iter = iter;
+			break;
+		}
+
+		xold = x;
+	}
+
+	return result;
+}
+
+// console.log(onePointIteration(-1, 0.000001, 'cos(x)'));
