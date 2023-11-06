35b34c5a15d27ced91455062a06d66a2d88c2591
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-11-06 22:23:34 +0700

    Added tooltips to code chart

--- src/lib/components/users/problem-type-bar/problem-type-bar.svelte
+++ src/lib/components/users/problem-type-bar/problem-type-bar.svelte
@@ -29,20 +29,27 @@
 	}, 0);
 </script>
 
-<div class="w-[12em] relative dark bg-background/10 h-2 rounded-full overflow-clip flex">
+<div
+	class="w-[12em] relative dark bg-background/10 h-2 rounded-full flex [&>*:first-child]:rounded-l-full [&>*:last-child]:rounded-r-full"
+>
 	{#if totalSize === 0}
 		<div class="h-full w-full" />
 	{/if}
 	{#each Object.keys(problemCount) as sl}
 		<div
-			class="h-full {getColor(sl)} relative"
+			class="h-full {getColor(sl)} relative group transition-all duration-300"
 			style="width: {((problemCount[sl] / totalSize) * 100).toFixed(2)}%"
 		>
-			<!-- <Card.Root class="absolute z-30 left-1/2 -translate-x-1/2 -top-12">
-				<Card.Content class="pb-0 py-2">
-					{ProblemData[sl].name} ({problemCount[sl]})
-				</Card.Content>
-			</Card.Root> -->
+			{#if sl in ProblemData}
+				<Card.Root
+					class="absolute z-30 left-1/2 -translate-x-1/2 -top-12 group-hover:opacity-100 opacity-0"
+				>
+					<Card.Content class="pb-0 py-2 whitespace-nowrap flex items-center">
+						<div class="w-2 h-2 mr-2 {getColor(sl)} rounded-full" />
+						{ProblemData[sl].name} ({problemCount[sl]})
+					</Card.Content>
+				</Card.Root>
+			{/if}
 		</div>
 	{/each}
 </div>
